8f786ab54f9a7b1c34c9f7f370795b5ee8ce883a
iseejunaid <junaidnadeem266@gmail.com>
2024-06-18 03:32:00 +0500

    final commit

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,15 @@
-const { firebaseDb } = require("../lib/firebase");
-const { collection, getDocs } = require("firebase/firestore");
-
+const { firebaseDb, firebaseStorage } = require("../lib/firebase");
+const {
+  collection,
+  getDocs,
+  where,
+  doc,
+  deleteDoc,
+  query,
+  updateDoc,
+} = require("firebase/firestore");
+const { ref, deleteObject, listAll } = require("firebase/storage");
+var nodemailer = require("nodemailer");
 var express = require("express");
 var router = express.Router();
 
@@ -46,9 +55,55 @@ router.get("/home", async function (req, res, next) {
           nationaId: doc.data().nationalID,
         });
       });
-    } else if (tab === "otherTab") {
-    } else {
-      data = [];
+    } else if (tab === "verified") {
+      const verifiedRef = collection(firebaseDb, "users");
+      const q = query(verifiedRef, where("isVerified", "==", true));
+      const snapshot = await getDocs(q);
+      snapshot.docs.forEach((doc) => {
+        const verificationDate = doc.data().verificationDate.toDate();
+        const formattedDate =
+          String(verificationDate.getDate()).padStart(2, "0") +
+          "-" +
+          String(verificationDate.getMonth() + 1).padStart(2, "0") +
+          "-" +
+          verificationDate.getFullYear();
+        data.push({
+          userId: doc.data().uid,
+          userName: doc.data().name,
+          country: doc.data().country,
+          email: doc.data().email,
+          phone: doc.data().phnNumber,
+          verificationDate: formattedDate,
+          dob: doc.data().dob,
+          nationaId: doc.data().nationalID,
+        });
+      });
+    } else if (tab == "rejected") {
+      const rejectedRef = collection(firebaseDb, "users");
+      const q = query(
+        rejectedRef,
+        where("verificationStatus", "==", "rejected")
+      );
+      const snapshot = await getDocs(q);
+      snapshot.docs.forEach((doc) => {
+        const verificationDate = doc.data().verificationDate.toDate();
+        const formattedDate =
+          String(verificationDate.getDate()).padStart(2, "0") +
+          "-" +
+          String(verificationDate.getMonth() + 1).padStart(2, "0") +
+          "-" +
+          verificationDate.getFullYear();
+        data.push({
+          userId: doc.data().uid,
+          userName: doc.data().name,
+          country: doc.data().country,
+          email: doc.data().email,
+          phone: doc.data().phnNumber,
+          verificationDate: formattedDate,
+          dob: doc.data().dob,
+          nationaId: doc.data().nationalID,
+        });
+      });
     }
 
     res.render("home", { tab, data });
@@ -58,6 +113,104 @@ router.get("/home", async function (req, res, next) {
   }
 });
 
+router.get("/verify", function (req, res, next) {
+  const { id, country, dob, nationalID, email } = req.query;
+  const userRef = collection(firebaseDb, "users");
+  const q = query(userRef, where("uid", "==", id));
+
+  getDocs(q)
+    .then((querySnapshot) => {
+      if (querySnapshot.empty) {
+        res.send("User does not exist");
+      } else {
+        const userDoc = querySnapshot.docs[0];
+
+        return updateDoc(userDoc.ref, {
+          isVerified: true,
+          verificationStatus: "",
+          country: country,
+          dob: dob,
+          email: email,
+          nationalID: nationalID,
+          verificationDate: new Date(),
+        }).then(() => {
+          const folderRef = ref(firebaseStorage, `VerificationImages/${id}`);
+          return listAll(folderRef)
+            .then((result) => {
+              const deletePromises = result.items.map((itemRef) => {
+                return deleteObject(itemRef);
+              });
+              return Promise.all(deletePromises);
+            })
+            .then(async () => {
+              const verificationDataRef = doc(
+                collection(firebaseDb, "verificationData"),
+                id
+              );
+              await deleteDoc(verificationDataRef);
+              sendMail(true, email);
+              res.redirect("/home?tab=newRequests");
+            })
+            .catch((error) => {
+              console.error("Error deleting folder contents:", error);
+              res.status(500).send("Error deleting folder contents");
+            });
+        });
+      }
+    })
+    .catch((error) => {
+      console.error("Error checking user existence: ", error);
+      res.status(500).send("Error checking user existence");
+    });
+});
+
+router.get("/reject", function (req, res, next) {
+  const { id, email } = req.query;
+  const userRef = collection(firebaseDb, "users");
+  const q = query(userRef, where("uid", "==", id));
+
+  getDocs(q)
+    .then((querySnapshot) => {
+      if (querySnapshot.empty) {
+        res.send("User does not exist");
+      } else {
+        const userDoc = querySnapshot.docs[0];
+
+        return updateDoc(userDoc.ref, {
+          isVerified: false,
+          verificationStatus: "rejected",
+          verificationDate: new Date(),
+        }).then(() => {
+          const folderRef = ref(firebaseStorage, `VerificationImages/${id}`);
+          return listAll(folderRef)
+            .then((result) => {
+              const deletePromises = result.items.map((itemRef) => {
+                return deleteObject(itemRef);
+              });
+              return Promise.all(deletePromises);
+            })
+            .then(async () => {
+              const verificationDataRef = doc(
+                collection(firebaseDb, "verificationData"),
+                id
+              );
+              await deleteDoc(verificationDataRef);
+              sendMail(false, email);
+              res.redirect("/home?tab=newRequests");
+            })
+            .catch((error) => {
+              console.error("Error deleting folder contents:", error);
+              res.status(500).send("Error deleting folder contents");
+            });
+        });
+      }
+    })
+    .catch((error) => {
+      console.error("Error checking user existence: ", error);
+      res.status(500).send("Error checking user existence");
+    });
+});
+
 router.get("/settings", function (req, res, next) {
   res.send("settings");
 });
@@ -65,5 +218,31 @@ router.get("/settings", function (req, res, next) {
 router.get("/logout", function (req, res, next) {
   res.redirect("/");
 });
+
+const sendMail = async (verified, receiver) => {
+  let testAccount = nodemailer.createTestAccount();
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    auth: {
+      user: "eladio.wuckert@ethereal.email",
+      pass: "cZHjMXmcGYsbtqME2e",
+    },
+  });
+
+  var subject = verified ? "Verification Approved" : "Verification Rejected";
+  var text = verified
+    ? "Your verification request has been approved. Please Log In again to get Full Experience."
+    : "Your verification request has been rejected";
+
+  let info = await transporter.sendMail({
+    from: "Healr <support.healr@mail.com>",
+    to: receiver,
+    subject: subject,
+    text: text,
+  });
+  console.log("Message sent: %s", info.messageId);
+};
 
 module.exports = router;
